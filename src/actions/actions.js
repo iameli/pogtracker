f6eb977862a74a05728937a26386127c01393707
dustwise <jared.mohney@gmail.com>
2017-07-24 10:10:44 -0600

    downloading all chunks, categorizing by library key

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,7 +3,7 @@ export const PARSE_CHAT = 'PARSE CHAT';
 //"https://rechat.twitch.tv/rechat-messages?client_id=7vaylot4y76nvfvl5smsdl3lbeiajs&start=0&video_id=v" + videoID, init
 
 export const parseChat = (videoID) => (dispatch, getState) => {
-  let startTime, endTime;
+  let startTime, endTime, currentTime;
   var request = new Request('https://api.twitch.tv/kraken/videos/' + videoID, {
     headers: new Headers({
       'Client-ID': '7vaylot4y76nvfvl5smsdl3lbeiajs'
@@ -14,25 +14,48 @@ export const parseChat = (videoID) => (dispatch, getState) => {
     .then(res => {
       return res.json();
     }).then(data => {
-        startTime = ((new Date(data.created_at)).getTime()) / 1000;
+        console.log(data);
+        startTime = ((new Date(data.recorded_at)).getTime()) / 1000;
         endTime = startTime + data.length;
+        currentTime = startTime;
+        const promises = [];
 
-        var request = new Request('https://cors-anywhere.herokuapp.com/https://rechat.twitch.tv/rechat-messages?&start='+startTime+"&video_id=v"+ videoID, {
-          headers: new Headers({
-            'Client-ID': '7vaylot4y76nvfvl5smsdl3lbeiajs'
+        while(currentTime <= endTime){
+          let request = new Request('https://cors-anywhere.herokuapp.com/https://rechat.twitch.tv/rechat-messages?&start='+currentTime+"&video_id=v"+videoID, {
+            headers: new Headers({
+              'Client-ID': '7vaylot4y76nvfvl5smsdl3lbeiajs'
+            })
+          });
+
+          promises.push(
+            fetch(request)
+          )
+          currentTime += 30;
+        }
+
+        return Promise.all(promises);
+    }).then(promises => {
+        return Promise.all(promises.map(promise => promise.json()));
+        // return promises.map(promise => {
+        //   return promise.json();
+        // });
+    }).then(jsons => {
+        const library = {}
+
+        //for every 30 second chunk
+        jsons.map(json => {
+        //create key based on first timestamp
+          library[json.data[0].attributes.timestamp] = { posts : [] };
+          let currentChunk = library[json.data[0].attributes.timestamp];
+
+          json.data.forEach(post => {
+            currentChunk.posts.push({
+              timestamp : post.attributes.timestamp,
+              message : post.attributes.message,
+              videoOffset : post.attributes["video-offset"]
+            });
           })
         });
-
-        return fetch(request)
-    }).then(res => {
-        return res.json();
-    }).then(data => {
-        const messages = [];
-        data.data.forEach(post => {
-          messages.push({
-            message: post.attributes.message,
-            timestamp: post.attributes.timestamp
-          });
-        });
+        console.log(library.sort())
     });
 }
--- src/reducers/appState.js
+++ src/reducers/appState.js
@@ -2,10 +2,13 @@ import * as actions from '../actions/actions';
 
 const initialState = {
   isParsingChat: false,
-  videoID: 160376138,
+  videoID: 159832455,
   pogs: [],
   bacon: true
 };
+
+//doc 160891561
+//beyondthesummit 159832455
 
 export default function appState(state=initialState, action){
   switch (action.type){
