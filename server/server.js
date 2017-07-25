2d6df67a696bad06da483208db059b9ff198f7c8
dustwise <jared.mohney@gmail.com>
2017-07-25 02:17:51 -0600

    split the route files

--- server/server.js
+++ server/server.js
@@ -1,19 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import router from './routes/api';
 
 const app = express();
-const router = express.Router();
 const port = 8080;
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
-router.get('/',(req, res) => {
-  res.json({message: 'What can I do for you?'})
-});
-
 app.use('/api', router);
 
-app.listen(port);
-console.log('Running on port: ' + 8080);
+app.listen(port, () => {
+  console.log('Running on port: ' + 8080);
+});
 
