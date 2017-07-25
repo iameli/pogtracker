180002fc380139add39830c50355ca5613bf1e05
dustwise <jared.mohney@gmail.com>
2017-07-25 01:52:39 -0600

    basics server running

--- server/server.js
+++ server/server.js
@@ -1,3 +1,19 @@
-import Express from 'express';
+import express from 'express';
+import bodyParser from 'body-parser';
 
-console.log("yo");
+const app = express();
+const router = express.Router();
+const port = 8080;
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+router.get('/',(req, res) => {
+  res.json({message: 'What can I do for you?'})
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Running on port: ' + 8080);
+
