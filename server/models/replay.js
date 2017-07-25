c78222be7d1dbe787efccd510708c83ea4ee534e
dustwise <jared.mohney@gmail.com>
2017-07-25 04:10:36 -0600

    restructured Replay model, replying with document if exists

--- server/models/replay.js
+++ server/models/replay.js
@@ -5,7 +5,11 @@ const ReplaySchema = new Schema({
   videoID : {
     type : Number,
     required : true
-  }
+  },
+  emotes : [{
+    name : String,
+    timings : [Number]
+  }]
 });
 
 const Replay = mongoose.model('Replay', ReplaySchema);
--- server/routes/api.js
+++ server/routes/api.js
@@ -15,15 +15,25 @@ router.get('/replay/:replay_id',(req, res) => {
  
     if(replay === null){
       const newReplay = new Replay();
-
       newReplay.videoID = req.params.replay_id;
+      newReplay.emotes = [
+        {
+          name : "Krappa",
+          timings : [100, 200, 300]
+        },
+        {
+          name : "PogCramp",
+          timings : [44, 55, 66]
+        }
+      ];
+      
       newReplay.save((err) => {
         err && res.send(err);
 
         res.json({ message: 'Replay added! Thanks for contributing!'});
       })
     } else {
-      res.json({ message : "appreciate it, but that already exists!"});
+      res.json(replay);
     }
   });
 });
