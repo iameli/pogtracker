baed64400f67bb07fe5519e20871a431588270eb
dustwise <jared.mohney@gmail.com>
2017-07-29 23:04:28 -0600

    path resolved

--- server/server.js
+++ server/server.js
@@ -13,6 +13,8 @@ const port = 8080;
 
 app.use(bodyParser.json());
 
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
